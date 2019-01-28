3225a26c7049a62ce7ae73894ca8d2f20f253fed
Chad Zirbel <crzirbel@gmail.com>
2019-01-28 15:25:13 -0600

    update outline and start working on tutorial

--- FW5121_Intro_to_R.R
+++ FW5121_Intro_to_R.R
@@ -11,8 +11,11 @@
 
 # outline -----------------------------------------------------------------
 #' ## Outline
-#' 0. R setup & tutorial 
+#' 0. R setup & tutorial
 #' 1. Pull in data
+#'    a. setwd()
+#'    b. R projects?
+#'    c. read.csv(), read.table(), others?
 #' 2. Re-organize, re-shape
 #' 3. Cleaning/ data view
 #' 4. Perform a calculation (by row, by column, by group, parallel narrative to Excel)
@@ -23,6 +26,24 @@
 
 
 # R setup & tutorial ------------------------------------------------------
+
+##Why use R?##
+# It's free
+# Language based (Reproducible)
+# Powerful data wrangling capabilities
+# flexible statistical analysis toolkit
+# Graphics and data visualization
+# powerful, cutting-edge analytics
+# A robust, vibrant community
+
+##R basics##
+
+# The different parts of R
+#' 1. The script
+#' 2. The Console
+#' 3. Environment
+#' 4. Files/Plots/Packages/Help
+
 
 
 
